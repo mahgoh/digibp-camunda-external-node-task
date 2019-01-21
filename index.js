31554b866c3c589cb21f21ca5b29ef67ed7cff61
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-01-21 20:43:36 +0100

    Update

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
     logger,
     Variables
   } = require("camunda-external-task-client-js");
-  var unirest = require("unirest");
+  const unirest = require("unirest");
+  const express = require('express')
+  const path = require('path')
+  const PORT = process.env.PORT || 5000
+
+  express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
   const client = new Client({
     baseUrl: "https://digibp-camunda.herokuapp.com/rest",
