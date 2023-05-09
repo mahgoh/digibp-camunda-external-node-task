ba8561d2bdac07fbe4871cff642ece7a81634f9e
Marco Kaufmann <marcokaufmann@malako.xyz>
2023-05-09 17:27:24 +0200

    fix: simplify and remove bpmn errors

--- DigiClient.js
+++ DigiClient.js
@@ -8,6 +8,31 @@ const client = new Client({
   asyncResponseTimeout: 29000,
 })
 
+client.subscribe(
+  'RequestPremium',
+  {
+    tenantIdIn: ['ENT5'],
+  },
+  async ({ task, taskService }) => {
+    const customerId = task.variables.get('CustomerID')
+
+    const estimatedPreminum = () => {
+      let premium = Math.floor(Math.random() * (200 - 100) + 100)
+      if (!customerId) premium *= 2
+      return premium
+    }
+
+    const processVariables = new Variables().set('EstimatedPremium', estimatedPreminum())
+
+    try {
+      taskService.complete(task, processVariables)
+      console.log('Premium was successfully estimated!!')
+    } catch (e) {
+      console.error(`Failed completing premium estimation`)
+    }
+  }
+)
+
 /**
  * EvaluateRisk
  * ------------
@@ -27,10 +52,6 @@ client.subscribe(
   },
   async ({ task, taskService }) => {
     const claimAmount = task.variables.getTyped('ClaimAmount')
-
-    // check if valid types
-    if (claimAmount.type !== 'float')
-      taskService.handleBpmnError(task, 'INVALID_TYPE', 'ClaimAmount has to be of type float.')
 
     // calculate risk
     let riskType = 'unknown'
@@ -77,28 +98,7 @@ client.subscribe(
     const policyId = task.variables.getTyped('PolicyID')
     const toBeStatus = task.variables.getTyped('Status')
 
-    const ALLOWED_STATUSES = ['pending', 'activated', 'deactivated']
-
-    // check if valid types
-    if (policyId.type !== 'integer')
-      taskService.handleBpmnError(task, 'INVALID_TYPE', 'PolicyID has to be of type integer.')
-
-    if (toBeStatus.type !== 'string' || !ALLOWED_STATUSES.includes(toBeStatus.value))
-      taskService.handleBpmnError(
-        task,
-        'INVALID_TYPE',
-        `Status has to be of type string and and of ${ALLOWED_STATUSES.map((s) => `"${s}"`).join(
-          ', '
-        )}.`
-      )
-
-    // change status
-    if (Math.random() < 0.01)
-      // 1% chance of failure
-      taskService.handleBpmnError(task, 'CHANGE_FAILED', 'Failed to change the policy status.')
-
     const processVariables = new Variables().set('Status', toBeStatus)
-
     try {
       taskService.complete(task, processVariables)
       console.log('I completed my task successfully!!')
@@ -115,82 +115,25 @@ client.subscribe(
   },
   async ({ task, taskService }) => {
     const invoiceNr = task.variables.get('InvoiceNr')
-    const invoiceAmount = task.variables.get('InvoiceAmount')
-    const issueDate = task.variables.get('IssueDate')
-    const dueDate = task.variables.get('DueDate')
-    const paymentReceived = task.variables.get('PaymentReceived')
 
-    if (!issueDate) {
-      issueDate = new Date()
-      dueDate = new Date(issueDate.getDate() + 30)
-      inDefault = false
-      paymentReceived = false
-      reminderSent = false
+    const issueDate = new Date()
+    const dueDate = new Date(issueDate.getDate() + 30)
+    const inDefault = false
+    const paymentReceived = false
+    const reminderSent = false
 
-      const processVariables = new Variables().setAll({
-        IssueDate: issueDate,
-        DueDate: dueDate,
-        InDefault: inDefault,
-        PaymentReceived: paymentReceived,
-        ReminderSent: reminderSent,
-      })
-      try {
-        taskService.complete(task, processVariables)
-        console.log('I completed my task successfully!!')
-      } catch (e) {
-        console.error(`Failed completing my task, ${e}`)
-      }
-    }
-
-    if (dueDate < new Date() && !paymentReceived) {
-      console.log('pretend to send reminder for invoice: ' + invoiceNr)
-      const processVariables = new Variables().setAll({
-        InDefault: true,
-        PaymentDeadlineMet: false,
-        ReminderSent: true,
-        InvoiceAmount: invoiceAmount + 20,
-      })
-      try {
-        taskService.complete(task, processVariables)
-        console.log('I completed my task successfully!!')
-      } catch (e) {
-        console.error(`Failed completing my task, ${e}`)
-      }
-    }
-  }
-)
-
-client.subscribe(
-  'RequestPremium',
-  {
-    tenantIdIn: ['ENT5'],
-  },
-  async ({ task, taskService }) => {
-    const agentId = task.variables.get('AgentID')
-    const productId = task.variables.get('ProductID')
-    const policyStartDate = task.variables.get('PolicyStartDate')
-    const policyEndDate = task.variables.get('PolicyEndDate')
-    const customerId = task.variables.get('CustomerID')
-    const prospectName = customerId === undefined ? task.variables.get('ProspectName') : undefined
-    const prospectSurname =
-      customerId === undefined ? task.variables.get('ProspectSurname') : undefined
-    const prospectAddress =
-      customerId === undefined ? task.variables.get('ProspectAddress') : undefined
-    const prospectEmail = customerId === undefined ? task.variables.get('ProspectEmail') : undefined
-
-    const estimatedPreminum = () => {
-      let premium = Math.floor(Math.random() * (200 - 100) + 100)
-      if (!customerId) premium *= 2
-      return premium
-    }
-
-    const processVariables = new Variables().set('EstimatedPremium', estimatedPreminum)
-
+    const processVariables = new Variables().setAll({
+      IssueDate: issueDate,
+      DueDate: dueDate,
+      InDefault: inDefault,
+      PaymentReceived: paymentReceived,
+      ReminderSent: reminderSent,
+    })
     try {
       taskService.complete(task, processVariables)
-      console.log('Premium was successfully estimated!!')
+      console.log('I completed my task successfully!!')
     } catch (e) {
-      console.error(`Failed completing premium estimation`)
+      console.error(`Failed completing my task, ${e}`)
     }
   }
 )
